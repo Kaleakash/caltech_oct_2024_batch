3c9245946d8b0d2f3ffecbfcb14fd9514ea667a3
Akash <akash300383@gmail.com>
2024-11-19 09:38:59 +0530

    done

--- Course 1 - CB FSD - Planning and UI Design/Projects/TypeScript Program/functionTypes.ts
+++ Course 1 - CB FSD - Planning and UI Design/Projects/TypeScript Program/functionTypes.ts
@@ -14,3 +14,23 @@ function addNumber(a:number,b:number){      // data type is number type consider
 addNumber(10,20);
 addNumber(10.10,20.20)
 //addNumber("A","B")
+
+// info function no return any type of value. 
+function info(): void {
+    console.log("info function")
+}
+info();
+
+// function with return string value 
+function sayHello(name:string):string {
+    return "Welcome user "+name;
+}
+let msg:string = sayHello("Steven");
+
+// function with any return type 
+function getInfo() :any {
+    //return 100;
+    //return true;
+    //return "Welcome"
+}
+
