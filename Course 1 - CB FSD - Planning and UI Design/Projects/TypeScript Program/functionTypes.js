4ee82cc85113d5eedda64db452263249b9eeb1b8
Akash <akash300383@gmail.com>
2024-11-19 09:04:25 +0530

    done

---
+++ Course 1 - CB FSD - Planning and UI Design/Projects/TypeScript Program/functionTypes.js
@@ -0,0 +1,16 @@
+// function with number of parameter 
+function add(a, b) {
+    console.log(a);
+    console.log(b);
+}
+add(10, 20);
+add("A", "B");
+//add(10)
+//add();
+function addNumber(a, b) {
+    console.log(a);
+    console.log(b);
+}
+addNumber(10, 20);
+addNumber(10.10, 20.20);
+//addNumber("A","B")
---
+++ Course 1 - CB FSD - Planning and UI Design/Projects/TypeScript Program/functionTypes.ts
@@ -0,0 +1,16 @@
+// function with number of parameter 
+function add(a,b){          // data type can be any type 
+    console.log(a)
+    console.log(b)
+}
+add(10,20)
+add("A","B")
+//add(10)
+//add();
+function addNumber(a:number,b:number){      // data type is number type consider. 
+    console.log(a)
+    console.log(b)
+}
+addNumber(10,20);
+addNumber(10.10,20.20)
+//addNumber("A","B")
