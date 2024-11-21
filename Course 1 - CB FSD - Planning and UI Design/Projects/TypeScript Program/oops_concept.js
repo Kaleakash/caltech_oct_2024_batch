eec2f81750b0534a019e82442cf4409637a7ce4f
Akash <akash300383@gmail.com>
2024-11-21 08:07:48 +0530

    done

--- Course 1 - CB FSD - Planning and UI Design/Projects/TypeScript Program/oops_concept.js
+++ Course 1 - CB FSD - Planning and UI Design/Projects/TypeScript Program/oops_concept.js
@@ -1,9 +1,16 @@
 var Employee = /** @class */ (function () {
-    function Employee() {
-        console.log("employee class object created..");
-        this.id = 100;
-        this.fname = "Steven";
-        this.salary = 45000;
+    // constructor() {
+    //     console.log("employee class object created..")
+    //     this.id=100;
+    //     this.fname="Steven";
+    //     this.salary=45000;
+    // }
+    function Employee(id, fname, salary) {
+        if (fname === void 0) { fname = "Unknown"; }
+        if (salary === void 0) { salary = 20000; }
+        this.id = id;
+        this.fname = fname;
+        this.salary = salary;
     }
     Employee.prototype.display = function () {
         console.log("display function");
@@ -13,5 +20,13 @@ var Employee = /** @class */ (function () {
     };
     return Employee;
 }());
-var emp1 = new Employee();
+var emp1 = new Employee(100, "Steven", 45000);
+var emp2 = new Employee(101, "Lex", 46000);
+var emp3 = new Employee(102, "John", 48000);
+var emp4 = new Employee(103, "Raj");
+var emp5 = new Employee(105);
 emp1.display();
+emp2.display();
+emp3.display();
+emp4.display();
+emp5.display();
--- Course 1 - CB FSD - Planning and UI Design/Projects/TypeScript Program/oops_concept.ts
+++ Course 1 - CB FSD - Planning and UI Design/Projects/TypeScript Program/oops_concept.ts
@@ -2,11 +2,16 @@ class Employee {
     id:number;
     fname:string;
     salary:number;
-    constructor() {
-        console.log("employee class object created..")
-        this.id=100;
-        this.fname="Steven";
-        this.salary=45000;
+    // constructor() {
+    //     console.log("employee class object created..")
+    //     this.id=100;
+    //     this.fname="Steven";
+    //     this.salary=45000;
+    // }
+    constructor(id:number,fname:string="Unknown",salary:number=20000){
+            this.id=id;
+            this.fname=fname;
+            this.salary=salary;
     }
     display() : void {
         console.log("display function")
@@ -15,6 +20,13 @@ class Employee {
         console.log("salary is "+this.salary)
     }
 }
-
-let emp1 = new Employee();
+let emp1 = new Employee(100,"Steven",45000);
+let emp2 = new Employee(101,"Lex",46000);
+let emp3 = new Employee(102,"John",48000);
+let emp4 = new Employee(103,"Raj")
+let emp5 = new Employee(105)
 emp1.display();
+emp2.display();
+emp3.display();
+emp4.display();
+emp5.display();
