6b10090bdfb1bf0851ab30edc76efee28eb49d35
Akash <akash300383@gmail.com>
2024-11-26 10:15:11 +0530

    done

--- Course 1 - CB FSD - Planning and UI Design/Projects/Angular Programs/angular-forms-service/src/app/mdf-login-page/mdf-login-page.component.ts
+++ Course 1 - CB FSD - Planning and UI Design/Projects/Angular Programs/angular-forms-service/src/app/mdf-login-page/mdf-login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-mdf-login-page',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class MdfLoginPageComponent {
 
+loginRef = new FormGroup({
+  emailid:new FormControl(),
+  password:new FormControl()
+});
+msg:string="";
+// these information we will check with help of back end technologies like java 
+checkLoginDetials(): void {
+  let login = this.loginRef.value;
+  if(login.emailid=="akash@gmail.com" && login.password=="123"){
+      this.msg="successfully login"
+  }else {
+    this.msg = "Failure try once again"
+  }
+  this.loginRef.reset();
+}
 }
