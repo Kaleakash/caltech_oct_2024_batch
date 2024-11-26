fe2481b778a6a0b20d34ecf813c9de3db0f0d217
Akash <akash300383@gmail.com>
2024-11-26 10:30:38 +0530

    done

--- Course 1 - CB FSD - Planning and UI Design/Projects/Angular Programs/angular-forms-service/src/app/mdf-login-page/mdf-login-page.component.ts
+++ Course 1 - CB FSD - Planning and UI Design/Projects/Angular Programs/angular-forms-service/src/app/mdf-login-page/mdf-login-page.component.ts
@@ -12,14 +12,20 @@ loginRef = new FormGroup({
   emailid:new FormControl(),
   password:new FormControl()
 });
+f1:boolean = false;
+f2:boolean = false;
 msg:string="";
 // these information we will check with help of back end technologies like java 
 checkLoginDetials(): void {
   let login = this.loginRef.value;
   if(login.emailid=="akash@gmail.com" && login.password=="123"){
       this.msg="successfully login"
+      this.f1=true;
+      this.f2=false;
   }else {
     this.msg = "Failure try once again"
+    this.f2=true;
+    this.f1=false;
   }
   this.loginRef.reset();
 }
