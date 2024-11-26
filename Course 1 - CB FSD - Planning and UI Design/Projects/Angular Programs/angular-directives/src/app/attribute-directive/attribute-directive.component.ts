33a6c148eaefb9730cf047d0b2a0937306da2339
Akash <akash300383@gmail.com>
2024-11-26 08:39:59 +0530

    done

--- Course 1 - CB FSD - Planning and UI Design/Projects/Angular Programs/angular-directives/src/app/attribute-directive/attribute-directive.component.ts
+++ Course 1 - CB FSD - Planning and UI Design/Projects/Angular Programs/angular-directives/src/app/attribute-directive/attribute-directive.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./attribute-directive.component.css']
 })
 export class AttributeDirectiveComponent {
-
+  styleValues={"background-color":"yellow"}
+  f1:boolean=true;
+  f2:boolean=false;
+  fun1(): void {
+      this.f1=!this.f1;
+  }
+  fun2() : void {
+    this.f2=!this.f2;
+  }
 }
