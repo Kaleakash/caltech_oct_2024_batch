f6d371f188668b70be45e441a29c56776298df3b
Akash <akash300383@gmail.com>
2024-11-28 10:30:23 +0530

    done

--- Course 1 - CB FSD - Planning and UI Design/Projects/Angular Programs/product-restapi/src/app/product.service.ts
+++ Course 1 - CB FSD - Planning and UI Design/Projects/Angular Programs/product-restapi/src/app/product.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from './product';
 @Injectable({
   providedIn: 'root'
 })
@@ -7,6 +9,7 @@ export class ProductService {
   constructor(public http:HttpClient) { }   // DI for HttpClient API. 
 
   loadFakeDataDisplayOnConsole(): void {
+    
     this.http.get("https://fakestoreapi.com/products").subscribe({
       
       next:(value:any)=> console.log(value),
@@ -16,5 +19,11 @@ export class ProductService {
       complete:()=>console.log("done!")
 
     })
+
+    console.log("Hello")
+    console.log("Hi")
+  }
+  loadFakeProductAndDisplayOnBrowser():Observable<Product[]>{
+    return this.http.get<Product[]>("https://fakestoreapi.com/products");
   }
 }
--- Course 1 - CB FSD - Planning and UI Design/Projects/Angular Programs/product-restapi/src/app/product.ts
+++ Course 1 - CB FSD - Planning and UI Design/Projects/Angular Programs/product-restapi/src/app/product.ts
@@ -1,2 +1,6 @@
-export interface Product {
+export interface Product {      // this interface is use to map json data 
+    id:number;
+    title:string;
+    price:number;
+    image:string;
 }
