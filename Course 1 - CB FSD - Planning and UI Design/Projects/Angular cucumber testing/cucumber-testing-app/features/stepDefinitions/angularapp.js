f1312b9fe72f52a322529d8331213c16d3c4a026
Akash <akash300383@gmail.com>
2024-12-03 10:29:59 +0530

    done

--- Course 1 - CB FSD - Planning and UI Design/Projects/Angular cucumber testing/cucumber-testing-app/features/stepDefinitions/angularapp.js
+++ Course 1 - CB FSD - Planning and UI Design/Projects/Angular cucumber testing/cucumber-testing-app/features/stepDefinitions/angularapp.js
@@ -1,9 +1,7 @@
 const {Then,Given,After}=require("@cucumber/cucumber");
 const {Builder,By,until} = require("selenium-webdriver");
 const assert = require("assert");       // it provide pre defined function to test the code
-
 let driver;
-
 Given('I Open the Angular Application', async function () {
     // Write code here that turns the phrase above into concrete actions
     driver = new  Builder().forBrowser('chrome').build();
@@ -11,14 +9,12 @@ Given('I Open the Angular Application', async function () {
     await driver.wait(until.elementLocated(By.id("h1Info")),10000);
     return 'done';
 });
-
 Then('I should see the welcome message {string}', async function (string) {
     // Write code here that turns the phrase above into concrete actions
     const text = await driver.findElement(By.id("h1Info")).getText();
     assert.equal(text,"Welcome to Angular App")
     return 'done';
 });
-
 After(async function () {
     try{
         await driver.quit();
