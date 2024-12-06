ce72d287c1aac0e8f87360a176f9a9435a476d87
Akash <akash300383@gmail.com>
2024-12-06 09:21:08 +0530

    done

---
+++ Course 2 - Backend and Database Development/Java Programs/JavaBasicPrograms/src/DataTypesExample.java
@@ -0,0 +1,19 @@
+
+public class DataTypesExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// let a;  JavaScript
+		// let a:number; TypeScript 
+		int a=10;	// In Java 
+		double b=10.20;
+		char c='$';
+		boolean d = true;
+		System.out.println(a);
+		System.out.println("Value of a is = "+a);
+		System.out.println("Value of b is = "+b);
+		System.out.println("Value of c is = "+c);
+		System.out.println("Value of d is = "+d);
+	}
+
+}
---
+++ Course 2 - Backend and Database Development/Java Programs/JavaBasicPrograms/src/Demo.java
@@ -0,0 +1,11 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java");
+		System.out.print("My Name is Akash");
+		System.out.println(" I am a Java Trainer");
+	}
+
+}
