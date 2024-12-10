17af9a4e149bf3af3ca63916004f652fc4bd1441
Akash <akash300383@gmail.com>
2024-12-10 07:54:13 +0530

    done

---
+++ Course 2 - Backend and Database Development/Java Programs/JavaBasicPrograms/src/ArrayExample.java
@@ -0,0 +1,23 @@
+
+public class ArrayExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		int abc[];
+		int b=10;
+		int num[]= {10,20,30,40,50,60,70,80};
+		System.out.println(b);
+		System.out.println(num);
+		System.out.println(num[0]);
+		System.out.println(num[1]);
+		int xyz[]=new int[5];	// created the memory 
+		System.out.println(xyz[0]);
+		xyz[0]=100;
+		xyz[1]=200;
+		//xyz[5]=500;
+		System.out.println(xyz[0]);
+		System.out.println(xyz[1]);
+	}
+
+}
