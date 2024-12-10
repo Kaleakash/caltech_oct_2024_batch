13171f528e60edd068895a9353356ec08a1ea01f
Akash <akash300383@gmail.com>
2024-12-10 09:58:52 +0530

    done

--- Course 2 - Backend and Database Development/Java Programs/JavaOOPsConcept/src/com/Account.java
+++ Course 2 - Backend and Database Development/Java Programs/JavaOOPsConcept/src/com/Account.java
@@ -7,10 +7,17 @@ float amount;
 
 void withdraw() {
 	System.out.println("Withdraw functionality ");
+	amount = amount-50;
 }
 
 void deposit() {
 	System.out.println("Deposit functionality ");
+	amount = amount+100;
+}
+
+void displayAccountInfo() {
+	System.out.println("Account details ");
+	System.out.println("Accno "+accno+" Name "+name+" Amount "+amount);
 }
 
 }
--- Course 2 - Backend and Database Development/Java Programs/JavaOOPsConcept/src/com/AccountTest.java
+++ Course 2 - Backend and Database Development/Java Programs/JavaOOPsConcept/src/com/AccountTest.java
@@ -4,12 +4,26 @@ public class AccountTest {
 
 	public static void main(String[] args) {
 		Account acc1 = new Account();		// memory created..
+		acc1.accno=1212;
+		acc1.name="Steven";
+		acc1.amount=1400;
+		acc1.displayAccountInfo();
 		acc1.withdraw();
 		acc1.deposit();
+		System.out.println("Afer transaction");
+		acc1.displayAccountInfo();
 
 		Account acc2 = new Account();		// separate memory created..
+		acc2.accno=1214;
+		acc2.name="John";
+		acc2.amount=1800;
+		acc2.displayAccountInfo();
 		acc2.deposit();
-		acc2.withdraw();
+		acc2.deposit();
+		System.out.println("Afer transaction");
+		acc2.displayAccountInfo();
+		//acc2.deposit();
+		//acc2.withdraw();
 	}
 
 }
