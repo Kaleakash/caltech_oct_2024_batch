ed552c3f2283eebccb59351dcb310ed56e919795
Akash <akash300383@gmail.com>
2024-12-17 08:50:39 +0530

    done

--- Course 2 - Backend and Database Development/Java Programs/PackageExample/src/education/main/App.java
+++ Course 2 - Backend and Database Development/Java Programs/PackageExample/src/education/main/App.java
@@ -1,12 +1,12 @@
 package education.main;
-//import education.school.Attendance;	// we can use Attendance class part of school package 
+import education.school.Attendance;	// we can use Attendance class part of school package 
 //import education.school.*;		// we can use all classes or interfaces part of school package 
 //import education.college.Attendance;
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		education.school.Attendance att1 = new education.school.Attendance();
+		Attendance att1 = new Attendance();
 		att1.display();
 		education.college.Attendance att2  = new education.college.Attendance();
 		att2.display();
