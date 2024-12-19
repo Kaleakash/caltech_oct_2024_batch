87cfa1496df897957e8f217068442353ce63c1ff
Akash <akash300383@gmail.com>
2024-12-19 08:14:17 +0530

    done

---
+++ Course 2 - Backend and Database Development/Java Programs/ExceptionHandlingExample/src/com/CompileTimeError.java
@@ -0,0 +1,11 @@
+package com;
+
+public class CompileTimeError {
+
+	public static void main(String[] args) {
+		int a;
+		System.out.println("Welcome to Java");
+		
+	}
+
+}
---
+++ Course 2 - Backend and Database Development/Java Programs/ExceptionHandlingExample/src/com/RunTimeError.java
@@ -0,0 +1,26 @@
+package com;
+
+public class RunTimeError {
+
+	public static void main(String[] args) {
+	System.out.println("Hi");
+	int a=10;
+	int b=0;
+	int num[]= {10,20,30,40,50};
+		try {
+		int result = a/b;
+		System.out.println("Result is "+result);
+		int result1 = num[3];
+		System.out.println("Result is "+result1);
+		}catch(Exception e) {
+			//System.out.println("I Take Care!");
+			//System.out.println(e.getMessage());  // display short message
+			System.out.println(e.toString());  // name of the exception with message
+		}
+		
+	System.out.println("Bye..");
+	System.out.println("Bye..");
+	System.out.println("Bye..");
+	}
+
+}
