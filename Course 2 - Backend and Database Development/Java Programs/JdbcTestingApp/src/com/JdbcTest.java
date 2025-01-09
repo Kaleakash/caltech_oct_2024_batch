f4721af9611fb939468c1f44591e44e585502222
Akash <akash300383@gmail.com>
2025-01-09 10:04:00 +0530

    done

--- Course 2 - Backend and Database Development/Java Programs/JdbcTestingApp/src/com/JdbcTest.java
+++ Course 2 - Backend and Database Development/Java Programs/JdbcTestingApp/src/com/JdbcTest.java
@@ -10,10 +10,29 @@ public class JdbcTest {
 	System.out.println("Connected successfully");
 	Statement stmt = con.createStatement();
 		// Insert Query 
-		int result = stmt.executeUpdate("insert into employee values(2,'John',25000)");
+		
+//		int result = stmt.executeUpdate("insert into employee values(2,'John',25000)");
+//		if(result>0) {
+//			System.out.println("Record inserted successfully");
+//		}
+		
+		// Update Query 
+		
+//		int result = stmt.executeUpdate("update employee set salary=48000 where eid=2");
+//		if(result>0) {
+//			System.out.println("Record updated successfully");
+//		}else {
+//			System.out.println("Record not present");
+//		}
+		
+		// Delete Query 
+		int result = stmt.executeUpdate("delete from employee where eid=2");
 		if(result>0) {
-			System.out.println("Record inserted successfully");
+			System.out.println("Record deleted successfully");
+		}else {
+			System.out.println("Record not present");
 		}
+		
 		} catch (Exception e) {
 			System.out.println(e.toString());
 		}
