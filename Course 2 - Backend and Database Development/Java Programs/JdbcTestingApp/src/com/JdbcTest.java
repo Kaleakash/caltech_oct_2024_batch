660c89b07f46c89bb10a914c24ef2692d8f2cdf5
Akash <akash300383@gmail.com>
2025-01-09 09:47:43 +0530

    done

--- Course 2 - Backend and Database Development/Java Programs/JdbcTestingApp/src/com/JdbcTest.java
+++ Course 2 - Backend and Database Development/Java Programs/JdbcTestingApp/src/com/JdbcTest.java
@@ -8,6 +8,12 @@ public class JdbcTest {
 		System.out.println("Driver loaded successfully");
 	Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "root@123");
 	System.out.println("Connected successfully");
+	Statement stmt = con.createStatement();
+		// Insert Query 
+		int result = stmt.executeUpdate("insert into employee values(2,'John',25000)");
+		if(result>0) {
+			System.out.println("Record inserted successfully");
+		}
 		} catch (Exception e) {
 			System.out.println(e.toString());
 		}
