7a51d85f1f68540bd794074d81082bd4beb9f97d
Akash <akash300383@gmail.com>
2025-01-17 09:43:59 +0530

    done

---
+++ Course 2 - Backend and Database Development/Java Web Application/LoginServletApp/src/main/java/com/LoginServlet.java
@@ -0,0 +1,47 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public LoginServlet() {
+        super();
+       
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();// it help to display the output on browser 
+		// request.getParameter("fieldName") which help to receive the value as string 
+		String emailid = request.getParameter("emailid");
+		String password = request.getParameter("password");
+		if(emailid.equals("akash@gmail.com") && password.equals("123")) {
+			pw.println("Successfully login! with Get Method");
+		}else {
+			pw.println("Failure try once again! with Method");
+		}
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();// it help to display the output on browser 
+		// request.getParameter("fieldName") which help to receive the value as string 
+		String emailid = request.getParameter("emailid");
+		String password = request.getParameter("password");
+		if(emailid.equals("akash@gmail.com") && password.equals("123")) {
+			pw.println("Successfully login! with Post method");
+		}else {
+			pw.println("Failure try once again! with Post method");
+		}
+	}
+
+}
