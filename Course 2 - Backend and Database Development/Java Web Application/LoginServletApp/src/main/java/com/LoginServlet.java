557c976c4f3cf88f17e2f2b0f6ed4a8e1e0a2c05
Akash <akash300383@gmail.com>
2025-01-17 10:15:48 +0530

    done

--- Course 2 - Backend and Database Development/Java Web Application/LoginServletApp/src/main/java/com/LoginServlet.java
+++ Course 2 - Backend and Database Development/Java Web Application/LoginServletApp/src/main/java/com/LoginServlet.java
@@ -3,6 +3,7 @@ package com;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,22 +27,34 @@ public class LoginServlet extends HttpServlet {
 		// request.getParameter("fieldName") which help to receive the value as string 
 		String emailid = request.getParameter("emailid");
 		String password = request.getParameter("password");
+		RequestDispatcher rd1 = request.getRequestDispatcher("Home");
+		RequestDispatcher rd2 = request.getRequestDispatcher("login.html");
+		
 		if(emailid.equals("akash@gmail.com") && password.equals("123")) {
 			pw.println("Successfully login! with Get Method");
+			rd1.forward(request, response);		// target page output. 
 		}else {
 			pw.println("Failure try once again! with Method");
+			rd2.include(request, response); 	// error message + login.html page code. 
 		}
+		response.setContentType("text/html");// set response as html format. 
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter pw = response.getWriter();// it help to display the output on browser 
 		// request.getParameter("fieldName") which help to receive the value as string 
 		String emailid = request.getParameter("emailid");
 		String password = request.getParameter("password");
+		RequestDispatcher rd1 = request.getRequestDispatcher("Home");
+		RequestDispatcher rd2 = request.getRequestDispatcher("login.html");
+		
 		if(emailid.equals("akash@gmail.com") && password.equals("123")) {
 			pw.println("Successfully login! with Post method");
+			rd1.forward(request, response);		// target page output. 
 		}else {
 			pw.println("Failure try once again! with Post method");
+			rd2.include(request, response); 	// error message + login.html page code. 
 		}
+		response.setContentType("text/html");// set response as html format. 
 	}
 
 }
