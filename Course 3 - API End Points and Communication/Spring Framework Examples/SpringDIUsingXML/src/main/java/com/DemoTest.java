43b7f599fe703c3528722694fa6cb95cdf05b77b
Akash <akash300383@gmail.com>
2025-02-21 09:53:32 +0530

    done

---
+++ Course 3 - API End Points and Communication/Spring Framework Examples/SpringDIUsingXML/src/main/java/com/DemoTest.java
@@ -0,0 +1,25 @@
+package com;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// according to core java we need to create the object 
+		// call method 
+		//Employee emp1 = new Employee();
+		//emp1.displayEmployee();
+		
+		// load the xml file 
+		Resource res = new ClassPathResource("beans.xml");
+		// BeanFactory object created 
+		BeanFactory fatory = new XmlBeanFactory(res);
+		Employee employee1 = (Employee)fatory.getBean("emp1");
+		employee1.displayEmployee();
+	}
+
+}
---
+++ Course 3 - API End Points and Communication/Spring Framework Examples/SpringDIUsingXML/src/main/java/com/Employee.java
@@ -0,0 +1,8 @@
+package com;
+
+public class Employee {
+
+	public void displayEmployee() {
+		System.out.println("This is Employee class method");
+	}
+}
