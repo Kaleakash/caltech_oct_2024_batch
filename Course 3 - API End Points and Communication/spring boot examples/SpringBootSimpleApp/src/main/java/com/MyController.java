b149b4118f7ebe7c497541f17d4f536e5cff8e1a
Akash <akash300383@gmail.com>
2025-03-13 08:43:39 +0530

    done

--- Course 3 - API End Points and Communication/spring boot examples/SpringBootSimpleApp/src/main/java/com/MyController.java
+++ Course 3 - API End Points and Communication/spring boot examples/SpringBootSimpleApp/src/main/java/com/MyController.java
@@ -1,5 +1,7 @@
 package com;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,7 +12,23 @@ public class MyController {
 	// http://localhost:9090
 			@RequestMapping(value = "/",method = RequestMethod.GET)
 			public String openPage() {
-				System.out.println("coding...");
+				System.out.println("Index page open");
 				return "index"; // spring boot check the page name inside a templates folder using the viewresolver concept. 
+			}
+			@RequestMapping(value = "/loginPage",method = RequestMethod.GET)
+			public String openLoginPage() {
+				System.out.println("login page open");
+				return "login";
+			}
+			@RequestMapping(value = "/SignIn",method = RequestMethod.POST)
+			public String checkLoginDetails(HttpServletRequest req) {   // DI for request object 
+				String emailid = req.getParameter("emailid");
+				String password = req.getParameter("password");
+				// we can check using jdbc or jdbcTempalte or JPA or spring jpa data
+				if(emailid.equals("akash@gmail.com") && password.equals("123")) {
+					return "success";		// success.html page 
+				}else {
+					return "failure";			// failure.html page 
+				}
 			}
 }
