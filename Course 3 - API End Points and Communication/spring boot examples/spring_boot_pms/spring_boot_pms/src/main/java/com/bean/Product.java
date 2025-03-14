56af3cf11746e97bb207821c7b55e3ba33bc095f
Akash <akash300383@gmail.com>
2025-03-14 07:47:33 +0530

    done

--- Course 3 - API End Points and Communication/spring boot examples/spring_boot_pms/spring_boot_pms/src/main/java/com/bean/Product.java
+++ Course 3 - API End Points and Communication/spring boot examples/spring_boot_pms/spring_boot_pms/src/main/java/com/bean/Product.java
@@ -1,11 +1,14 @@
 package com.bean;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Product {
 @Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)		// it is use to create auto_increment features 
 private int pid;
 private String pname;
 private float price;
