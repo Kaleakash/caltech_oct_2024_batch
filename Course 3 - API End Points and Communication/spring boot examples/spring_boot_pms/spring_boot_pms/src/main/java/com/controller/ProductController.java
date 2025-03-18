60cb39adba7447e9e2acc5f6fab1a865172cb130
Akash <akash300383@gmail.com>
2025-03-18 07:44:30 +0530

    done

--- Course 3 - API End Points and Communication/spring boot examples/spring_boot_pms/spring_boot_pms/src/main/java/com/controller/ProductController.java
+++ Course 3 - API End Points and Communication/spring boot examples/spring_boot_pms/spring_boot_pms/src/main/java/com/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,8 +22,10 @@ public class ProductController {
 	public String openIndexPage(Model mm,Product product) {  // DI for model api as well as Product class 
 		System.out.println("Open Index Page");
 		mm.addAttribute("name", "Akash Kale"); // like a request.setAttribute("name","akash kale");
-		mm.addAttribute("pp", product);  // we store product object in model scope
+			mm.addAttribute("pp", product);  // we store product object in model scope
 		mm.addAttribute("project", "Product Management System");
+		List<Product> listOfProducts = productService.findAllProducts();
+		mm.addAttribute("products", listOfProducts);    // we store list of product in model scope with key as products 
 		return "index";
 	}
 	
@@ -32,14 +36,17 @@ public class ProductController {
 		// we need to do type casting with respective data type of variable. 
 		// we need to convert those value to java bean object and pass to service layer 
 		String result = productService.storeProduct(product);
-		product.setPid(0);
-		product.setPname("");
-		product.setPrice(0.0f);		// reset value not to display once again on same form. 
+//		product.setPid(0);
+//		product.setPname("");
+//		product.setPrice(0.0f);		// reset value not to display once again on same form. 
 		//System.out.println(product.toString());  // we check pname and price set or not
 		mm.addAttribute("name", "Akash Kale"); // like a request.setAttribute("name","akash kale");
 		mm.addAttribute("pp", product);  // we store product object in model scope
 		mm.addAttribute("project", "Product Management System");
 		mm.addAttribute("msg", result);
+		List<Product> listOfProducts = productService.findAllProducts();
+		mm.addAttribute("products", listOfProducts);    // we store list of product in model scope with key as products 
 		return "index";
 	}
 }
+
--- Course 3 - API End Points and Communication/spring boot examples/spring_boot_pms/spring_boot_pms/src/main/java/com/service/ProductService.java
+++ Course 3 - API End Points and Communication/spring boot examples/spring_boot_pms/spring_boot_pms/src/main/java/com/service/ProductService.java
@@ -1,7 +1,7 @@
 package com.service;
 
 import java.util.List;
-
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,4 +26,38 @@ public class ProductService {
 			return productDao.findAll();	// findAll pre defined retrieve all product as list of product
 			
 		}
+		
+		public String deleteProduct(int pid) {
+			if(productDao.existsById(pid)) {		// if record present return true else return false.
+				productDao.deleteById(pid);	// no return type means void 
+				return "product record deleted";
+			}else {
+				return "product not present";
+			}
+		}
+		
+		public String updateProduct(Product product) {		// pid = 1, pname = "TV", price = 55000
+			
+			
+			Optional<Product> result = productDao.findById(product.getPid());
+			
+			if(result.isPresent()) {
+					Product p = result.get();	// retrieve that product object from db. 
+					p.setPname(product.getPname());
+					p.setPrice(product.getPrice());  // replace new data. 
+					productDao.saveAndFlush(p);		// update existing record with new information 
+					return "Product updated successfully";
+			}else {
+				return "product not present";
+			}
+		}
 }
+
+
+
+
+
+
+
+
+
