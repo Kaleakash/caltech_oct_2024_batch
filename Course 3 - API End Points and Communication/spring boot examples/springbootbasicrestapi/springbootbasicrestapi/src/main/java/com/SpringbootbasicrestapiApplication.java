70fa27bb06cc314ab08bca49c95222d3414791be
Akash <akash300383@gmail.com>
2025-03-20 07:35:57 +0530

    done

---
+++ Course 3 - API End Points and Communication/spring boot examples/springbootbasicrestapi/springbootbasicrestapi/src/main/java/com/SpringbootbasicrestapiApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbasicrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootbasicrestapiApplication.class, args);
+	}
+
+}
