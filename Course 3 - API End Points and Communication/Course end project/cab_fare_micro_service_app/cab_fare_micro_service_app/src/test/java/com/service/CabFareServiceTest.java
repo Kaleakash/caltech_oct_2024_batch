50c3a66a2636a9dec99daac8cf385020316ef96a
Akash <akash300383@gmail.com>
2025-04-03 08:33:59 +0530

    done

--- Course 3 - API End Points and Communication/Course end project/cab_fare_micro_service_app/cab_fare_micro_service_app/src/test/java/com/service/CabFareServiceTest.java
+++ Course 3 - API End Points and Communication/Course end project/cab_fare_micro_service_app/cab_fare_micro_service_app/src/test/java/com/service/CabFareServiceTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.bean.CabFare;
+
 @SpringBootTest
 class CabFareServiceTest {
 
@@ -16,16 +18,22 @@ class CabFareServiceTest {
 	@Test
 	@Disabled
 	void testStoreCabFareInfo() {
-		fail("Not yet implemented");
+		CabFare cf = new CabFare();
+		cf.setSource("D");
+		cf.setDestination("E");
+		cf.setFare(18);
+		String result = cabFareService.storeCabFareInfo(cf);
+		assertEquals("Car Fare Information stored", result);
 	}
 
 	@Test
+	//@Disabled
 	void testFindFare() {
 	float fare1 = cabFareService.findFare("A", "B");
 	assertEquals(10.0, fare1,2);		// 3rd parameter after decimal 2 point 
 	
 	float fare2 = cabFareService.findFare("B", "C");
-	assertEquals(-1, fare2);
+	assertEquals(20.0, fare2);
 	//assertEquals(null, null);
 	}
 
