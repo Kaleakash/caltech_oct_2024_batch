0ff0eae64a84f05b61f9dea4168c8a30315979df
Akash <akash300383@gmail.com>
2025-04-10 08:04:54 +0530

    done

---
+++ Course 4 -  Integration and Deployment/Docker Images/Image to run Java Program/Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+    public static void main(String args[]) {
+        System.out.println("Welcome to Java Program created by Akash");
+        int a=10;
+        int b=20;
+        int sum = a+b;
+        System.out.println("Sum of a and b is: " + sum);
+    }
+}
